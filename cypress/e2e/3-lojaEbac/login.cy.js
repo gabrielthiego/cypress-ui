5ca485e078783122ea65255497aa71c88ff71e6d
Gabriel Thiego <gabrielthiego@gmail.com>
2024-08-12 20:15:34 -0300

    teste autopmatizado

---
+++ cypress/e2e/3-lojaEbac/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+    describe('funcionalidade login', () => {
+         it('deve fazer login com sucesso', () => {
+            cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+            cy.get('#username').type('gabrielteste@teste.com.br')
+            cy.get('#password').type('teste@123')
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, .gabriel.thiegogabriel.thiegogabriel.thiego (não é .gabriel.thiegogabriel.thiegogabriel.thiego? Sair)')
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+         })
+
+
+    })
